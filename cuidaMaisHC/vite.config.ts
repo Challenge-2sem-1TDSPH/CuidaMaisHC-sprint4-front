8e3d55b98b19078d89a6d38c931566056c15820c
devsbrana <sbranaww@gmail.com>
2025-11-03 09:05:27 -0300

    fix: corrigindo vite config

--- cuidaMaisHC/vite.config.ts
+++ cuidaMaisHC/vite.config.ts
@@ -4,10 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cuidamaishc"
+  base: "/cuidamaishc",
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  base: "/cuidaMaisHC"
+  ]
 })
